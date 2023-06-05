6bf5aa2a7104c0f9375504484d936897225cdd03
Bruno Andrade <119361832+BrunoAnndrade@users.noreply.github.com>
2023-06-05 16:03:57 -0300

    add fun expression

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +12,35 @@ class MainActivity : AppCompatActivity() {
 
         // esconder a actionbar
         supportActionBar!!.hide()
+
+
+        numero_zero.setOnClickListener { acrescentarUmaExpressao("0", true) }
+        numero_um.setOnClickListener { acrescentarUmaExpressao("1", true) }
+        numero_dois.setOnClickListener { acrescentarUmaExpressao("2", true) }
+        numero_tres.setOnClickListener { acrescentarUmaExpressao("3", true) }
+        numero_quatro.setOnClickListener { acrescentarUmaExpressao("4", true) }
+        numero_cinco.setOnClickListener { acrescentarUmaExpressao("5", true) }
+        numero_seis.setOnClickListener { acrescentarUmaExpressao("6", true) }
+        numero_sete.setOnClickListener { acrescentarUmaExpressao("7", true) }
+        numero_oito.setOnClickListener { acrescentarUmaExpressao("8", true) }
+        numero_nove.setOnClickListener { acrescentarUmaExpressao("9", true) }
+
+
     }
+
+    fun acrescentarUmaExpressao(string: String, limpar: Boolean) {
+
+        if (resultado.text.isNotEmpty()) {
+            expressao.text = ""
+        }
+        if (limpar) {
+            resultado.text = ""
+            expressao.append(string)
+        } else {
+            expressao.append(string)
+            resultado.append(string)
+        }
+    }
+
+
 }
